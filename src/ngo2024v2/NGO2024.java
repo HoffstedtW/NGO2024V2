e942dce75e6469b794f7a8b208b19da1a8693d2d
HoffstedtW <hoffstedtwilliam@gmail.com>
2024-05-08 11:38:48 +0200

    +

--- src/ngo2024v2/NGO2024.java
+++ src/ngo2024v2/NGO2024.java
@@ -21,7 +21,7 @@ public class NGO2024 {
     public static void main(String[] args) {
         
         try{
-            idb = new InfDB("ngo_2024", "3306", "root", "admin");
+            idb = new InfDB("ngo_2024", "3306", "dbAdmin2024", "dbAdmin2024PW");
             new Inloggning(idb).setVisible(true);
         
         } catch (InfException ex){
