25be09a95f8709177c59df5fd29b7738579c7aa8
HoffstedtW <hoffstedtwilliam@gmail.com>
2024-05-08 11:35:49 +0200

    +

--- src/ngo2024v2/Inloggning.java
+++ src/ngo2024v2/Inloggning.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package ngo.pkg2024;
+package ngo2024v2;
 
 import oru.inf.InfDB;
 import oru.inf.InfException;
