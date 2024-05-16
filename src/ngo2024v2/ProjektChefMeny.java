7909bb0cddefd9d02db49958542c11f3ef3a43a7
Jgustavsson02 <jerrygustavsson02@gmail.com>
2024-05-16 12:42:17 +0200

    +

--- src/ngo2024v2/ProjektChefMeny.java
+++ src/ngo2024v2/ProjektChefMeny.java
@@ -18,10 +18,10 @@ public class ProjektChefMeny extends javax.swing.JFrame {
     /**
      * Creates new form ProjektChefMeny
      */
-    public ProjektChefMeny(InfDB idb, String aid) {
+    public ProjektChefMeny(InfDB idb, String InloggadProjektChef) {
         initComponents();
         this.idb = idb;
-        this.InloggadHandlaggare = InloggadHandlaggare;
+        this.InloggadHandlaggare = InloggadProjektChef;
         this.setVisible(rootPaneCheckingEnabled);
         
     }
@@ -44,14 +44,14 @@ public class ProjektChefMeny extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        VälkomnenProjektChef = new javax.swing.JLabel();
         btnVisaPartners = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setText("jLabel1");
 
-        jLabel2.setText("jLabel2");
+        VälkomnenProjektChef.setText("jLabel2");
 
         btnVisaPartners.setText("Visa Partners");
         btnVisaPartners.addActionListener(new java.awt.event.ActionListener() {
@@ -66,7 +66,7 @@ public class ProjektChefMeny extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(169, 169, 169)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(VälkomnenProjektChef, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(194, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
@@ -79,7 +79,7 @@ public class ProjektChefMeny extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2)
+                .addComponent(VälkomnenProjektChef)
                 .addGap(53, 53, 53)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
@@ -100,41 +100,20 @@ public class ProjektChefMeny extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ProjektChefMeny.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ProjektChefMeny.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ProjektChefMeny.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ProjektChefMeny.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+   public static void main(String args[]) {
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            // Skapa en ny instans av ProjektChefMeny och visa den
+            InfDB idb = null;
+            String InloggadProjektChef = null;
+            new ProjektChefMeny(idb, InloggadProjektChef).setVisible(true);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ProjektChefMeny().setVisible(true);
-            }
-        });
-    }
+    });
+}
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel VälkomnenProjektChef;
     private javax.swing.JButton btnVisaPartners;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
