a6efa46f77ea804ccffc987ada472b929ad7c63b
HoffstedtW <hoffstedtwilliam@gmail.com>
2024-05-21 16:09:54 +0200

    +

--- src/ngo2024v2/Inloggning.java
+++ src/ngo2024v2/Inloggning.java
@@ -39,6 +39,7 @@ public class Inloggning extends javax.swing.JFrame {
         lblFelmeddelande = new javax.swing.JLabel();
         btnLoggaIn = new javax.swing.JButton();
         pfLosenord = new javax.swing.JPasswordField();
+        btnGlomtLosenord = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -68,6 +69,13 @@ public class Inloggning extends javax.swing.JFrame {
             }
         });
 
+        btnGlomtLosenord.setText("Glömt Lösenord");
+        btnGlomtLosenord.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGlomtLosenordActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -81,10 +89,14 @@ public class Inloggning extends javax.swing.JFrame {
                             .addComponent(lblLosenord)
                             .addComponent(lblEpost, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(27, 27, 27)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(pfLosenord)
-                            .addComponent(btnLoggaIn)
-                            .addComponent(tfEpost, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(pfLosenord)
+                                .addComponent(tfEpost, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(btnGlomtLosenord)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(21, 21, 21)
+                                .addComponent(btnLoggaIn)))))
                 .addContainerGap(50, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -100,7 +112,9 @@ public class Inloggning extends javax.swing.JFrame {
                     .addComponent(pfLosenord, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(33, 33, 33)
                 .addComponent(btnLoggaIn)
-                .addGap(34, 34, 34)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnGlomtLosenord)
+                .addGap(5, 5, 5)
                 .addComponent(lblFelmeddelande)
                 .addContainerGap(75, Short.MAX_VALUE))
         );
@@ -167,6 +181,12 @@ public class Inloggning extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_pfLosenordActionPerformed
 
+    private void btnGlomtLosenordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGlomtLosenordActionPerformed
+        // TODO add your handling code here:
+        GlömtLösenord GlomtLosenordFönster = new GlömtLösenord(idb);
+        GlomtLosenordFönster.setVisible(true);
+    }//GEN-LAST:event_btnGlomtLosenordActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -203,6 +223,7 @@ public class Inloggning extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGlomtLosenord;
     private javax.swing.JButton btnLoggaIn;
     private javax.swing.JLabel lblEpost;
     private javax.swing.JLabel lblFelmeddelande;
