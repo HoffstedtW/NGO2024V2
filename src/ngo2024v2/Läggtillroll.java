464ae284b3cf60e5a2a521f1da82877adf490faf
HoffstedtW <hoffstedtwilliam@gmail.com>
2024-05-22 15:17:36 +0200

    +

--- src/ngo2024v2/Läggtillroll.java
+++ src/ngo2024v2/Läggtillroll.java
@@ -35,6 +35,7 @@ public class Läggtillroll extends javax.swing.JFrame {
         this.dispose();
         LäggtillAnställd LäggtillAnställdFönster = new LäggtillAnställd(idb, InloggadAdmin);
         LäggtillAnställdFönster.setVisible(true);
+        
     }
 
     
@@ -55,6 +56,7 @@ public class Läggtillroll extends javax.swing.JFrame {
         btnSpara = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         txtAID = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -78,6 +80,8 @@ public class Läggtillroll extends javax.swing.JFrame {
 
         jLabel2.setText("AID");
 
+        jLabel3.setText("Lägg till roll");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -86,13 +90,16 @@ public class Läggtillroll extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 217, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 217, Short.MAX_VALUE)
                                 .addComponent(btnSpara)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(btnGåtillbaka))
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2)))
                         .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(ComboRoll, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -106,7 +113,9 @@ public class Läggtillroll extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
                 .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
@@ -175,6 +184,7 @@ public class Läggtillroll extends javax.swing.JFrame {
     private javax.swing.JButton btnSpara;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JTextField txtAID;
     // End of variables declaration//GEN-END:variables
 }
